988ff92875e9e2a63eed3e0e8c37ff82f4daf3f9
Jorge Iraizoz <jorgeirai27@gmail.com>
2026-05-27 14:51:51 +0200

    Load schemas and create master build script

--- 2_DW_Mart_Build/01_create_tables_dw.sql
+++ 2_DW_Mart_Build/01_create_tables_dw.sql
@@ -14,7 +14,7 @@ CREATE TABLE company_dim (
 
 CREATE TABLE skills_dim (
     skill_id    INTEGER     PRIMARY KEY,
-    skill       VARCHAR,
+    skills       VARCHAR,
     type        VARCHAR
 );
 
@@ -29,7 +29,7 @@ CREATE TABLE job_postings_fact (
     job_schedule_type       VARCHAR,
     job_work_from_home      BOOLEAN,
     search_location         VARCHAR,
-    job_poted_date          TIMESTAMP,
+    job_posted_date          TIMESTAMP,
     job_no_degree_mention   BOOLEAN,
     job_health_insurance    BOOLEAN,
     job_country             VARCHAR,
